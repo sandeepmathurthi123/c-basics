d3ed0ad9a45f02e96544d68665a5630113e5126e
sandeepmathurthi123 <93192878+sandeepmathurthi123@users.noreply.github.com>
2021-10-30 12:02:54 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+	//variable declaration
+    int age;
+	float cgpa;
+	char initial;
+	//initialization
+	age=21;
+	cgpa=8.0;
+	initial='M';
+	//output
+	printf("%d\n",age);
+	printf("%f\n",cgpa);
+	printf("%c, initial");	
+}
